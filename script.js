f7eef607d1f9391075eaa47274feb1c0bed45707
Your Name <giannhskeram@gmail.com>
2023-06-20 18:23:18 +0300

    add  leafmap

--- script.js
+++ script.js
@@ -18,7 +18,22 @@ if (navigator.geolocation) {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
       console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
+
+      const lat = [latitude, longitude];
+
+      const map = L.map('map').setView(lat, 13);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(lat)
+        .addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
     },
+
     function () {
       alert('could not get your position');
     }
