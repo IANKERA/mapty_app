29685ca86248ba333ac093dd56ed0fa8115f8cdb
Your Name <giannhskeram@gmail.com>
2023-06-21 08:22:06 +0300

    handling clicks

--- script.js
+++ script.js
@@ -11,7 +11,9 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-if (navigator.geolocation) {
+let map, mapEvent;
+
+if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
     function (position) {
       console.log(position);
@@ -19,23 +21,45 @@ if (navigator.geolocation) {
       const { longitude } = position.coords;
       console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
 
-      const lat = [latitude, longitude];
+      const coords = [latitude, longitude];
 
-      const map = L.map('map').setView(lat, 13);
+      map = L.map('map').setView(coords, 13);
 
       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(lat)
-        .addTo(map)
-        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-        .openPopup();
+      //handling clicks
+      map.on('click', function (mapE) {
+        mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+      });
     },
 
     function () {
       alert('could not get your position');
     }
   );
-}
+
+form.addEventListener('sumbit', function (e) {
+  e.preventDefault();
+  //display maker
+  console.log(mapEvent);
+  const { lat, lng } = mapEvent.latlng;
+
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Workout')
+    .openPopup();
+});
